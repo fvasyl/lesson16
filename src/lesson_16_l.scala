1c1370209e483b96fae0ed3ee41669f57a0cd65b
vfushtei <f.v.1214.for.friends@gmail.com>
2018-12-11 02:09:05 +0200

    fibonacci_0

--- src/lesson_16_l.scala
+++ src/lesson_16_l.scala
@@ -1,7 +1,9 @@
 object HelloWorld {
   def main(args: Array[String]): Unit = {
+
     println(s"Command line arguments: ${args.toList}")
     println(s"Grouped command line arguments: ${args.toList.grouped(2).toList}")
+
     case class Person(name: String, age: Int)
     val people = args.grouped(2).map { pair =>
       val name = pair(0)
@@ -22,6 +24,21 @@ object HelloWorld {
     val byAge = people.sortBy(_.age)
 
     println(s"People sorted by age: ${byAge}")
+
+    def fact(n: Int): Int = {
+      if (n<=0) 1
+      else {
+        fact(n-1)+fact(n-2)
+      }
+    }
+    println(fact(0))
+    println(fact(1))
+      println(fact(2))
+      println(fact(3))
+      println(fact(4))
+      println(fact(5))
+      println(fact(6))
+      println(fact(7))
 
   }
 }
